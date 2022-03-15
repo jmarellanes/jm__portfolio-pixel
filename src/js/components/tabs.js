ddf9cd4ba96b242e844bc719cc5ccbb7fae9ddd9
jmarellanes <dev.jmarellanes@gmail.com>
2022-03-15 04:27:20 -0700

    Add tabs.js script to change tabs with keyboard

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,5 +1,6 @@
 function tabs() {
   const isActive = 'is-active';
+  const sectionList = document.querySelectorAll('.home__content > section');
 
   const updateAttributes = (el, boolean, number) => {
     el.firstElementChild.setAttribute('aria-selected', boolean);
@@ -25,7 +26,6 @@ function tabs() {
   const handleClick = (e) => {
     const buttonTarget = e.target;
     const buttonTargetParent = buttonTarget.parentNode; // li element
-    const sectionList = document.querySelectorAll('.home__content > section');
 
     const stopFunction =
       buttonTarget.tagName.toLowerCase() !== 'button' ||
@@ -39,8 +39,53 @@ function tabs() {
     changeActiveSection(liTargetIndex, sectionList);
   };
 
+  const handleKeyDown = (e) => {
+    let UP_ARROW = 'ArrowUp',
+      DOWN_ARROW = 'ArrowDown',
+      LEFT_ARROW = 'ArrowLeft',
+      RIGHT_ARROW = 'ArrowRight';
+
+    const itemActive = document.activeElement.parentNode;
+    const tabEl = Array.from(itemActive.parentNode.children);
+    const firstTabEl = tabEl[0].firstElementChild;
+    const lastTabEl = tabEl[tabEl.length - 1].firstElementChild;
+    const liTargetIndex = tabEl.indexOf(itemActive);
+
+    switch (e.key) {
+      case RIGHT_ARROW:
+      case DOWN_ARROW:
+        if (document.activeElement === lastTabEl) {
+          changeActiveButton(0, tabEl);
+          changeActiveSection(0, sectionList);
+          tabEl[0].firstElementChild.focus();
+        } else {
+          changeActiveButton(liTargetIndex + 1, tabEl);
+          changeActiveSection(liTargetIndex + 1, sectionList);
+          tabEl[liTargetIndex + 1].firstElementChild.focus();
+        }
+        break;
+
+      case LEFT_ARROW:
+      case UP_ARROW:
+        if (document.activeElement === firstTabEl) {
+          changeActiveButton(tabEl.length - 1, tabEl);
+          changeActiveSection(tabEl.length - 1, sectionList);
+          tabEl[tabEl.length - 1].firstElementChild.focus();
+        } else {
+          changeActiveButton(liTargetIndex - 1, tabEl);
+          changeActiveSection(liTargetIndex - 1, sectionList);
+          tabEl[liTargetIndex - 1].firstElementChild.focus();
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
   const navEl = document.querySelector('.nav-main__element');
   navEl.addEventListener('click', handleClick);
+  document.addEventListener('keydown', handleKeyDown);
 }
 
 export { tabs };
