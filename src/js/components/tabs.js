ed18b86146c4524506a5a3c50af31b5b8b9dbea8
jmarellanes <dev.jmarellanes@gmail.com>
2022-03-26 12:49:53 -0700

    Refactor tabs script

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -2,29 +2,36 @@ function tabs() {
   const isActive = 'is-active';
   const isFading = 'is-fading';
   const sectionList = document.querySelectorAll('.home__content > section');
+  const buttonsMenuList = Array.from(
+    document.querySelectorAll('button.nav-main__button')
+  );
 
-  const changeActiveButton = (i, array) => {
-    function updateAttributes(el, isSelected, value) {
-      el.firstElementChild.setAttribute('aria-selected', isSelected);
-      el.firstElementChild.setAttribute('tabindex', value);
+  const changeActiveButton = (targetIndex, array = buttonsMenuList) => {
+    function updateAttributes(element, isSelected, value) {
+      element.setAttribute('aria-selected', isSelected);
+      element.setAttribute('tabindex', value);
     }
 
     array.forEach((element, index) => {
-      index === i
+      index === targetIndex
         ? updateAttributes(element, true, 0)
         : updateAttributes(element, false, -1);
     });
   };
 
-  const changeActiveSection = (i, nodeList, activeIndex) => {
+  const changeActiveSection = (
+    targetIndex,
+    prevActiveIndex,
+    nodeList = sectionList
+  ) => {
     nodeList.forEach((element, index) => {
-      if (index === i) {
+      if (index === targetIndex) {
         element.classList.remove(isFading);
         setTimeout(() => {
           element.classList.add(isActive);
           element.removeAttribute('aria-hidden');
         }, 250);
-      } else if (index === activeIndex) {
+      } else if (index === prevActiveIndex) {
         element.classList.add(isFading);
         element.classList.remove(isActive);
         element.setAttribute('aria-hidden', true);
@@ -37,23 +44,22 @@ function tabs() {
 
   const handleClick = (e) => {
     const buttonTarget = e.target;
-    const buttonTargetParent = buttonTarget.parentNode; // li element
 
-    const stopFunction =
+    const stopEvent =
       buttonTarget.tagName.toLowerCase() !== 'button' ||
       buttonTarget.getAttribute('aria-selected') === 'true';
-    if (stopFunction) return;
+    if (stopEvent) return;
 
-    const liArray = Array.from(buttonTargetParent.parentElement.children);
-    const liActive = liArray.find(
-      (el) => el.firstElementChild.getAttribute('aria-selected') === 'true'
+    const elPrevActive = buttonsMenuList.find(
+      (button) => button.getAttribute('aria-selected') === 'true'
     );
-    const liActiveIndex = liArray.indexOf(liActive);
-    const liTargetIndex = liArray.indexOf(buttonTargetParent);
 
-    changeActiveButton(liTargetIndex, liArray);
+    const elPrevActiveIndex = buttonsMenuList.indexOf(elPrevActive);
+    const elTargetIndex = buttonsMenuList.indexOf(buttonTarget);
+
+    changeActiveButton(elTargetIndex);
     setTimeout(() => {
-      changeActiveSection(liTargetIndex, sectionList, liActiveIndex);
+      changeActiveSection(elTargetIndex, elPrevActiveIndex);
     }, 200);
   };
 
@@ -66,31 +72,21 @@ function tabs() {
     // prettier-ignore
     let keyPressed = e.key === UP_ARROW || e.key === DOWN_ARROW || e.key === LEFT_ARROW || e.key === RIGHT_ARROW;
 
-    const liFocusActive = document.activeElement.parentNode;
-    if (!keyPressed || !liFocusActive.classList.contains('tabs__nav-item')) {
+    const activeElement = document.activeElement;
+    if (!keyPressed || activeElement.tagName.toLocaleLowerCase() !== 'button') {
       return;
     }
 
-    const liArray = Array.from(liFocusActive.parentNode.children);
-    const firstElement = liArray[0].firstElementChild;
-    const lastElement = liArray[liArray.length - 1].firstElementChild;
-    const liBeforeActive = liArray.find(
-      (el) => el.firstElementChild.getAttribute('aria-selected') === 'true'
-    );
-    const liActiveIndex = liArray.indexOf(liBeforeActive);
-    const liTargetIndex = liArray.indexOf(liFocusActive);
+    const firstElement = buttonsMenuList[0];
+    const lastElement = buttonsMenuList[buttonsMenuList.length - 1];
+    const elTargetIndex = buttonsMenuList.indexOf(activeElement);
 
-    function updateElements(
-      index,
-      array = liArray,
-      nodeList = sectionList,
-      activeIndex = liActiveIndex
-    ) {
+    function updateElements(index, array = buttonsMenuList) {
       changeActiveButton(index, array);
       setTimeout(() => {
-        changeActiveSection(index, nodeList, activeIndex);
+        changeActiveSection(index, elTargetIndex);
       }, 200);
-      array[index].firstElementChild.focus();
+      array[index].focus();
     }
 
     switch (e.key) {
@@ -98,14 +94,14 @@ function tabs() {
       case DOWN_ARROW:
         document.activeElement === lastElement
           ? updateElements(0)
-          : updateElements(liTargetIndex + 1);
+          : updateElements(elTargetIndex + 1);
         break;
 
       case LEFT_ARROW:
       case UP_ARROW:
         document.activeElement === firstElement
-          ? updateElements(liArray.length - 1)
-          : updateElements(liTargetIndex - 1);
+          ? updateElements(buttonsMenuList.length - 1)
+          : updateElements(elTargetIndex - 1);
         break;
 
       default:
@@ -113,7 +109,7 @@ function tabs() {
     }
   };
 
-  const navEl = document.querySelector('.nav-main__element');
+  const navEl = document.querySelector('.nav-main__container');
   navEl.addEventListener('click', handleClick);
   document.addEventListener('keydown', handleKeyDown);
 }
